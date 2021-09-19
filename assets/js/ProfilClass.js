4bea70e0f90ce89efa28e07af6d2d1dd1038b0e7
Godrade <jorisr@hotmail.fr>
2021-09-19 23:18:43 +0200

    ADD Profil

--- assets/js/ProfilClass.js
+++ assets/js/ProfilClass.js
@@ -1,8 +1,5 @@
 class ProfilClass  {
 
-    constructor() {
-    }
-
     //Recovery json data
     getJson = async (tag = null) => { 
         const response = await fetch('assets/data/data.json')
@@ -25,8 +22,8 @@ class ProfilClass  {
         data = await data;
 
         if(tag){
-            let test = document.querySelectorAll('.user-item');
-            test.forEach(element => {document.getElementById(element.id).remove()})
+            let userDomItem = document.querySelectorAll('.user-item');
+            userDomItem.forEach(element => {document.getElementById(element.id).remove()})
         }
 
         data['photographers'].forEach(element => {
@@ -37,7 +34,7 @@ class ProfilClass  {
             divElt.id = `user-item-${element.id}` 
     
             let aElt = document.createElement('a');
-            aElt.href = `profil/?id=${element.id}`;
+            aElt.href = `profil.html?id=${element.id}`;
             aElt.id = `link-profil-${element.id}`;
     
             let divAfterLink = document.createElement('div');
@@ -97,12 +94,119 @@ class ProfilClass  {
         });
     }
 
+    //Update indexDOM for Tag
     updateIndexDomForTag = async (tag) => {
         let data = await this.getJson(tag);
         this.createIndexDynamicDom({'photographers' : data}, true);
     }
 
-    createProfilDynamicDom = async (data) => {
-        console.log(await data['id'][243]);
+    createProfilDynamicDom = async (data, userID) => {
+        const dataJson = await data;
+
+        //Get User by ID
+        let user = [];
+        dataJson['photographers'].forEach(element => {
+            if (element.id == userID){
+                user.push(element)
+            }
+        })
+
+        //Update DOM Element
+        document.getElementById('name').textContent = user[0]['name'];
+        document.getElementById('contact-name').textContent = user[0]['name'];
+        document.getElementById('city').textContent = user[0]['city'];
+        document.getElementById('desc').textContent = user[0]['tagline'];
+        document.getElementById('img-profil').src = `assets/profil/Photographers ID Photos/${user[0]['portrait']}`
+
+        let i = 0;
+        user[0]['tags'].forEach(element => {
+            
+            let divTag = document.createElement('div');
+            divTag.id = `tag-${i}`;
+            divTag.className = 'tag';
+            document.getElementById(`user-tag`).appendChild(divTag);
+
+            let buttonTag = document.createElement('button');
+            buttonTag.textContent = element;
+            document.getElementById(`tag-${i}`).appendChild(buttonTag);
+            i++;
+        })
+
+
+        //get Media by UserID
+        let media = [];
+        dataJson['media'].forEach(element => {
+            if (element.photographerId == userID){
+                media.push(element)
+            }
+        })
+
+
+        //Created DOM for media Elements
+        let name = user[0]['name'].split(' ')[0].replace('-', ' ');
+        media.forEach(element => {
+            let divItem = document.createElement('div');
+            divItem.className = 'portfolio-item';
+            divItem.id = `item-${element.id}`;
+
+                let videoElt = document.createElement('video');
+                videoElt.setAttribute('controls', 'true');
+                videoElt.id = `video-${element.id}`
+
+                let sourceElt = document.createElement('source');
+                sourceElt.src = `assets/profil/${name}/${element.video}`;
+                sourceElt.type = 'video/mp4';
+
+                let imageElt = document.createElement('img');
+                imageElt.src = `assets/profil/${name}/${element.image}`;
+                imageElt.alt = `Photo de ${user[0]['name']}`;
+
+            let divItemInfo = document.createElement('div');
+            divItemInfo.className = 'profil-item-info';
+            divItemInfo.id = `info-${element.id}`
+
+            let pItemName = document.createElement('p');
+            pItemName.className = "portfolio-name";
+            pItemName.id = `name-${element.id}`
+            pItemName.textContent = element.title;
+
+            let pItemlike = document.createElement('p');
+            pItemlike.className = "portfolio-name";
+            pItemlike.id = `like-${element.id}`
+            pItemlike.textContent = `${element.likes} <3`;
+
+            document.getElementById("portfolio").appendChild(divItem);
+            
+            if (element.video){
+                document.getElementById(`item-${element.id}`).appendChild(videoElt);
+                document.getElementById(`video-${element.id}`).appendChild(sourceElt);
+            } else {
+                document.getElementById(`item-${element.id}`).appendChild(imageElt);
+            }
+
+            document.getElementById(`item-${element.id}`).appendChild(divItemInfo);
+            document.getElementById(`info-${element.id}`).appendChild(pItemName);
+            document.getElementById(`info-${element.id}`).appendChild(pItemlike);
+
+        })
     }
+
+    verifiedElements() {
+        console.clear();
+        const form = document.querySelector('form');
+
+        //Get value input
+        const firstName = form.elements.form_firstName.value.trim();
+        const lastName = form.elements.form_lastName.value.trim();
+        const email = form.elements.form_email.value;
+        const message = form.elements.form_message.value;
+
+        console.log('----------');
+        console.log('Prénom:', firstName);
+        console.log('Nom:', lastName);
+        console.log('Email:', email);
+        console.log('Message:', message);
+        console.log('----------');
+    }
+
 }
--- assets/js/controller.js
+++ assets/js/controller.js
@@ -7,8 +7,44 @@ switch(url){
         const tagList = document.querySelectorAll("button");
         tagList.forEach((element) => element.addEventListener("click",() => profil.updateIndexDomForTag(element.id)));
     break;
-    case '/profil/':
-        profil.createProfilDynamicDom(profil.getJson());
+    case '/profil.html':
+        profil.createProfilDynamicDom(profil.getJson(), window.location.href.split('?id=')[1]);
+
+        // DOM Elements
+        const modalbg = document.querySelector(".bground");
+        const modalBtn = document.querySelectorAll(".modal-btn");
+        const form = document.getElementById("form");
+
+        // launch modal event
+        modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+        //Close modal
+        const modalClose = document.querySelectorAll(".close");
+        const btnModalClose = document.getElementById("close-modal");
+        modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
+
+
+        function launchModal() {
+            form.reset();
+            document.body.classList.add('open-modal');
+            form.style.display = "block";
+            modalbg.style.display = "block";
+        }
+    
+        function closeModal() {
+            document.body.classList.remove('open-modal');
+            modalbg.style.display = "none";
+        }
+
+        //Verified elements
+        function validate() {
+            profil.verifiedElements();
+        }
+
+        form.addEventListener("submit", function (e) {
+            e.preventDefault();
+            validate();
+        });
         break;
         default:
             console.log("Error page not found");
